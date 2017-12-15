dc9fa9a739322f9fb52e6771620b9dc1eff25f79
JW Janse <jaco.janse@trivident.com>
2017-12-15 17:02:29 +0100

    Added unit test project

--- Kulana.Common.Columns/TopBottomDistribution.cs
+++ Kulana.Common.Columns/TopBottomDistribution.cs
@@ -7,7 +7,7 @@ namespace Kulana.Common.Columns
         public int GetColumnIndex(int totalColumns, int totalItems, int currentItemIndex)
         {
             int itemsPerColumn = (int)Math.Ceiling(totalItems / (double)totalColumns);
-            int currentColumn = (currentItemIndex / itemsPerColumn) + 1;
+            int currentColumn = (currentItemIndex / itemsPerColumn);
             return currentItemIndex % totalColumns; 
         }
     }
